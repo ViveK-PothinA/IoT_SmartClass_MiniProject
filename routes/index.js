2978d495b5d3f9d7f06e3e4110e6b6cb5165e85c
ViveK-PothinA <kevivthapion@gmail.com>
2018-09-07 01:10:01 +0530

    mostly-done

--- routes/index.js
+++ routes/index.js
@@ -1,42 +1,76 @@
 var express = require('express');
 var router = express.Router();
 
+var data = {'0':'0', '1':'1', '2':'1','3':'1'};
+
 router.get('/', function(req, res, next) {
-  res.send(`
-  <span>Hello there! This is the root URL. These are the end-points:</span>
-    <ul>
-    <li>
-      <form action="/registerDevice" method="post">
-        <input type="hidden" name="device" value="0" />
-        <input type="hidden" name="val" value="0" />
-        <button type="submit">
-          <strong>POST</strong> <code>/registerDevice (device = 0, val = 0)</code>
-        </button>
-      </form>
-    </li>
-    <li>
-      <form action="/updateDevice" method="post">
-        <input type="hidden" name="device" value="0" />
-        <input type="hidden" name="val" value="1" />
-        <button type="submit">
-          <strong>POST</strong> <code>/updateDevice (device = 0, val = 1)</code>
-        </button>
-      </form>
-    </li>
-    <li>
-      <form action="/getStatus/0" method="get">
-        <input type="hidden" name="device" value="0" />
-        <button type="submit">
-          <strong>GET</strong> <code>/getStatus (device = 0)</code>
-        </button>
-      </form>
-    </li>
+  // res.send(`
+  // <span>Hello there! This is the root URL. These are the end-points:</span>
+  //   <ul>
+  //   <li>
+  //     <form action="/registerDevice" method="post">
+  //       <input type="hidden" name="device" value="0" />
+  //       <input type="hidden" name="val" value="0" />
+  //       <button type="submit">
+  //         <strong>POST</strong> <code>/registerDevice (device = 0, val = 0)</code>
+  //       </button>
+  //     </form>
+  //   </li>
+  //   <li>
+  //     <form action="/updateDevice" method="post">
+  //       <input type="hidden" name="device" value="0" />
+  //       <input type="hidden" name="val" value="1" />
+  //       <button type="submit">
+  //         <strong>POST</strong> <code>/updateDevice (device = 0, val = 1)</code>
+  //       </button>
+  //     </form>
+  //   </li>
+  //   <li>
+  //     <form action="/getStatus/0" method="get">
+  //       <input type="hidden" name="device" value="0" />
+  //       <button type="submit">
+  //         <strong>GET</strong> <code>/getStatus (device = 0)</code>
+  //       </button>
+  //     </form>
+  //   </li>
       
-  </ul>
-  `)
+  // </ul>
+  // `)
+  // console.log(data.length());
+  console.log("final");
+  console.log(data);
+  res.render('index', {data: data});
+
 });
 
-var data = {'0':'0', '1':'0'};
+router.post("/updateAllDevices", function(req, res){
+  // req.body = JSON.parse(JSON.stringify(req.body));
+  console.log(req.body);
+  var on = [];
+  var s = "";
+  for(var i in req.body){
+    console.log(i.slice(7,-1));
+    if(!data.hasOwnProperty(i.slice(7))){
+      s = s + i.slice(7) + ",";
+    }
+    else{
+      on.push(i.slice(7));
+    }
+  }
+  console.log(data, on);
+  for(var i in data){
+    console.log("a", i, on.includes(i));
+    if(on.includes(i)){
+      data[i] = '1';
+    }
+    else{
+      data[i] = '0';
+    }
+  }
+  on = [];
+  console.log(data);
+  res.status(200).redirect("/");
+});
 
 router.post("/registerDevice", function(req, res){
   req.body = JSON.parse(JSON.stringify(req.body));
@@ -53,7 +87,7 @@ router.post("/registerDevice", function(req, res){
 
   data[device] = req.body.val;
   res.status(200).send(`<span>`+device+` device has been initialized with a value of `+req.body.val);
-  console.log(data);
+  // console.log(data);
 });
 
 router.post("/updateDevice", function(req, res){
